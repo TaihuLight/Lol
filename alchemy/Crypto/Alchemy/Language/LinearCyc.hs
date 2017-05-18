72f8c85b9f7490342842e5e697a6d499bcd6d113
Chris Peikert <cpeikert@alum.mit.edu>
2017-05-17 21:38:12 -0400

    generalized LinearCyc interface

--- alchemy/Crypto/Alchemy/Language/LinearCyc.hs
+++ alchemy/Crypto/Alchemy/Language/LinearCyc.hs
@@ -12,28 +12,23 @@ import GHC.Exts                       (Constraint)
 
 -- | Symantics for evaluating a linear function on cyclotomics.
 
-class LinearCyc expr rep where
+class LinearCyc expr linrep cycrep where
 
   -- | Constraints needed to linear
-  type LinearCycCtx
-         expr
-         (rep :: * -> *)
-         (t :: Factored -> * -> *)
-         (e :: Factored)
-         (r :: Factored)
-         (s :: Factored)
-         zp :: Constraint
+  type LinearCycCtx expr linrep cycrep
+         (e :: Factored) (r :: Factored) (s :: Factored) zp :: Constraint
 
   -- | 'Cyc' wrapper for the input to linearing
-  type PreLinearCyc expr rep :: * -> *
+  type PreLinearCyc expr cycrep :: Factored -> * -> *
 
   -- | An object-language expression representing the given linear function.
-  linearCyc_ :: (LinearCycCtx expr rep t e r s zp)
-    => Linear t zp e r s
-    -> expr env ((PreLinearCyc expr rep) (Cyc t r zp) -> rep (Cyc t s zp))
+  linearCyc_ :: (LinearCycCtx expr linrep cycrep e r s zp)
+    => linrep e r s zp
+    -> expr env (PreLinearCyc expr cycrep r zp -> cycrep s zp)
 
-linearCyc :: (LinearCyc expr rep, LinearCycCtx expr rep t e r s zp, Lambda expr)
-  => Linear t zp e r s
-  -> expr env ((PreLinearCyc expr rep) (Cyc t r zp))
-  -> expr env (rep (Cyc t s zp))
+linearCyc :: (LinearCyc expr linrep cycrep,
+              LinearCycCtx expr linrep cycrep e r s zp, Lambda expr)
+  => linrep e r s zp
+  -> expr env (PreLinearCyc expr cycrep r zp)
+  -> expr env (cycrep s zp)
 linearCyc f a = linearCyc_ f $: a
