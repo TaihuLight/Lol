8d9ba655cc10f446ed9c65166f94fc4b48c1f2ea
Chris Peikert <cpeikert@alum.mit.edu>
2017-06-21 16:56:05 -0400

    updated to Arrow style

--- alchemy/Crypto/Alchemy/Language/List.hs
+++ alchemy/Crypto/Alchemy/Language/List.hs
@@ -1,14 +1,16 @@
 
 module Crypto.Alchemy.Language.List where
 
+import Crypto.Alchemy.Language.Lambda
+
 -- | Symantics for list operations.
 
-class List expr where
+class Lambda expr => List expr where
   -- | Empty list.
   nil_  :: expr e [a]
 
   -- | List constructor (analogue of ':').
-  cons_ :: expr e (a -> [a] -> [a])
+  cons_ :: expr e (Arrow expr a (Arrow expr [a] [a]))
 
 {- CJP: Keeping these out for now.  They aren't useful until we have a
 way to compare against nil, and have conditionals in the language.
