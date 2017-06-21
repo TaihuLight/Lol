32eaffc84f5b0e131cb755e5a1f58aeef9702327
Chris Peikert <cpeikert@alum.mit.edu>
2017-06-21 16:49:59 -0400

    updated to Arrow style; Lambda superclass of all

--- alchemy/Crypto/Alchemy/Language/Arithmetic.hs
+++ alchemy/Crypto/Alchemy/Language/Arithmetic.hs
@@ -7,14 +7,14 @@ import Crypto.Alchemy.Language.Lambda
 
 -- | Addition.
 
-class Add expr a where
+class (Lambda expr) => Add expr a where
   -- | Addition.
-  add_ :: expr e (a -> a -> a)
+  add_ :: expr e (Arrow expr a (Arrow expr a a))
   -- | Negation.
-  neg_ :: expr e (a -> a)
+  neg_ :: expr e (Arrow expr a a)
 
 infixl 6 +:, -:
-(+:), (-:) :: (Add expr a, Lambda expr) => expr e a -> expr e a -> expr e a
+(+:), (-:) :: (Add expr a) => expr e a -> expr e a -> expr e a
 
 -- | Convenient metalanguage version of 'add_'.
 a +: b = add_ $: a $: b
@@ -25,36 +25,36 @@ a -: b = a +: (neg_ $: b)
 -- | Addition of (metalanguage) literals to (object language)
 -- expressions.
 
-class AddLit expr a where
-  addLit_ :: a -> expr e (a -> a)
+class (Lambda expr) => AddLit expr a where
+  addLit_ :: a -> expr e (Arrow expr a a)
 
 infixl 6 >+:
-(>+:) :: (AddLit expr a, Lambda expr) => a -> expr e a -> expr e a
+(>+:) :: (AddLit expr a) => a -> expr e a -> expr e a
 a >+: b = addLit_ a $: b
 
 -- | Multiplication. (Note that the input type @b@ may differ from the
 -- output type @a@.)
 
-class Mul expr a where
+class (Lambda expr) => Mul expr a where
   type PreMul expr a
 
   -- | Multiplication.
-  mul_ :: expr e (PreMul expr a -> PreMul expr a -> a)
+  mul_ :: expr e (Arrow expr (PreMul expr a) (Arrow expr (PreMul expr a) a))
 
 -- | Convenient metalanguage version of 'mul'.
 infixl 7 *:                     -- match Haskell's precedence
-(*:) :: (Mul expr a, Lambda expr) =>
+(*:) :: (Mul expr a) =>
         expr e (PreMul expr a) -> expr e (PreMul expr a) -> expr e a
 a *: b = mul_ $: a $: b
 
 -- | Multiplication of (metalanguage) literals to (object language)
 -- expressions.
 
-class MulLit expr a where
-  mulLit_ :: a -> expr e (a -> a)
+class (Lambda expr) => MulLit expr a where
+  mulLit_ :: a -> expr e (Arrow expr a a)
 
 infixl 7 >*:
-(>*:) :: (MulLit expr a, Lambda expr) => a -> expr e a -> expr e a
+(>*:) :: (MulLit expr a) => a -> expr e a -> expr e a
 a >*: b = mulLit_ a $: b
 
 -- | Symantics for division-by-2 of a known-to-be-even value along
@@ -65,4 +65,4 @@ class Div2 expr a where
 
   -- | Divide a value that is known to be even, along with its integer
   -- modulus, by two.
-  div2_ :: expr e (PreDiv2 expr a -> a)
+  div2_ :: expr e (Arrow expr (PreDiv2 expr a) a)
