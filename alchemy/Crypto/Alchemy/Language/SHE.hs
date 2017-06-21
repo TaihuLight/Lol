fde7c1465c41ab0f0fbd6fa75db6d423e2211a36
Chris Peikert <cpeikert@alum.mit.edu>
2017-06-21 17:09:41 -0400

    updated to Arrow style

--- alchemy/Crypto/Alchemy/Language/SHE.hs
+++ alchemy/Crypto/Alchemy/Language/SHE.hs
@@ -4,6 +4,8 @@
 
 module Crypto.Alchemy.Language.SHE where
 
+import Crypto.Alchemy.Language.Lambda
+
 import Crypto.Lol                      (Cyc, Factored)
 import Crypto.Lol.Applications.SymmSHE (CT, KSQuadCircHint, SK, TunnelHint)
 import GHC.Exts
@@ -11,7 +13,7 @@ import GHC.Exts
 -- | Symantics for somewhat-homomorphic encryption operations (not
 -- including those defined in 'Crypto.Alchemy.Language.Arithmetic').
 
-class SHE expr where
+class Lambda expr => SHE expr where
 
   type ModSwitchPTCtx   expr ct zp' :: Constraint
   type ModSwitchCtx     expr ct zq' :: Constraint
@@ -26,23 +28,23 @@ class SHE expr where
     zp zq gad :: Constraint
 
   modSwitchPT_ :: (ModSwitchPTCtx expr ct zp', ct ~ CT m zp (Cyc t m' zq))
-    => expr env (ct -> CT m zp' (Cyc t m' zq))
+    => expr env (Arrow expr ct (CT m zp' (Cyc t m' zq)))
 
   modSwitch_ :: (ModSwitchCtx expr ct zq', ct ~ CT m zp (Cyc t m' zq))
-    => expr env (ct -> CT m zp (Cyc t m' zq'))
+    => expr env (Arrow expr ct (CT m zp (Cyc t m' zq')))
 
   addPublic_ :: (AddPublicCtx expr ct, ct ~ CT m zp (Cyc t m' zq))
-    => Cyc t m zp -> expr env (ct -> ct)
+    => Cyc t m zp -> expr env (Arrow expr ct ct)
 
   mulPublic_ :: (MulPublicCtx expr ct, ct ~ CT m zp (Cyc t m' zq))
-    => Cyc t m zp -> expr env (ct -> ct)
+    => Cyc t m zp -> expr env (Arrow expr ct ct)
 
   keySwitchQuad_ :: (KeySwitchQuadCtx expr ct gad, ct ~ CT m zp (Cyc t m' zq))
-    => KSQuadCircHint gad (Cyc t m' zq) -> expr env (ct -> ct)
+    => KSQuadCircHint gad (Cyc t m' zq) -> expr env (Arrow expr ct ct)
 
   tunnel_ :: (TunnelCtx expr t e r s e' r' s' zp zq gad)
     => TunnelHint gad t e r s e' r' s' zp zq
-    -> expr env (CT r zp (Cyc t r' zq) -> CT s zp (Cyc t s' zq))
+    -> expr env (Arrow expr (CT r zp (Cyc t r' zq)) (CT s zp (Cyc t s' zq)))
 
 -- | Symantics for obtaining the error rate of a ciphertext.
 
@@ -53,4 +55,4 @@ class ErrorRate expr where
   -- | Error rate of a ciphertext.  (Note that the secret key lives
   -- "outside" the object language.)
   errorRate_ :: (ErrorRateCtx expr ct z, ct ~ CT m zp (Cyc t m' zq))
-             => SK (Cyc t m' z) -> expr e (ct -> Double)
+             => SK (Cyc t m' z) -> expr e (Arrow expr ct Double)
