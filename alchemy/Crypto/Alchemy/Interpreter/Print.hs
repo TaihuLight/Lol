2d19af16699f4cc7523b2c5ba00de9ba80018e93
Chris Peikert <cpeikert@alum.mit.edu>
2017-06-22 23:49:08 -0400

    update to object-language style

--- alchemy/Crypto/Alchemy/Interpreter/Print.hs
+++ alchemy/Crypto/Alchemy/Interpreter/Print.hs
@@ -31,75 +31,88 @@ newtype P e a = P { unP :: Int -> String }
 pprint :: P () a -> String
 pprint = flip unP 0
 
-pureP :: String -> P e a
-pureP = P . const
+pprint1  :: String -> P e a           -> P e b
+pprint2  :: String -> P e a  -> P e b -> P e c
+pprint2' :: String -> String -> P e a -> P e b
+pprintIn :: String -> P e a  -> P e b -> P e c
+
+pprint1  op a   = P $ \i -> "(" ++ op ++ " " ++ unP a i ++ ")"
+pprint2  op a b = P $ \i -> "(" ++ op ++ " " ++ unP a i ++ " " ++ unP b i ++ ")"
+pprint2' op a b = P $ \i -> "(" ++ op ++ " " ++ a       ++ " " ++ unP b i ++ ")"
+pprintIn op a b = P $ \i -> "(" ++ unP a i ++ " " ++ op ++ " " ++ unP b i ++ ")"
 
 -- | In the printout, variable indices grow "outside in," rather than
 -- "inside out" (as in object-language code) because the
 -- implementation is simpler that way.
 
 instance Lambda P where
-  lam f  = P $ \i -> "(\\v" ++ show  i ++ " -> " ++ unP f (i+1) ++ ")"
-  f $: a = P $ \i -> "("    ++ unP f i ++ " "    ++ unP a i     ++ ")"
-  v0     = P $ \i -> "v" ++ show (i-1)
-  s  v   = P $ \i -> unP v (i-1)
+  data Arrow P a b              -- unused, so empty type
+  lam f = P $ \i -> "(\\v" ++ show  i ++ " -> " ++ unP f (i+1) ++ ")"
+  ($:)  = pprintIn ""
+  v0    = P $ \i -> "v" ++ show (i-1)
+  s  v  = P $ \i -> unP v (i-1)
 
 instance List P where
-  nil_  = pureP "nil"
-  cons_ = pureP "cons"
+  nil_  = P $ const "nil"
+  cons_ = pprint2 "cons"
 
 instance Add P a where
-  add_ = pureP "add"
-  neg_ = pureP "neg"
+  (+:) = pprintIn "+"
+  neg_ = pprint1  "neg"
 
 instance Show a => AddLit P a where
-  addLit_ a = pureP $ "addLit (" ++ show a ++ ")"
+  (>+:) a = pprint2' ">+" (show a)
 
 instance Mul P a where
   type PreMul P a = a
-  mul_ = pureP "mul"
+  (*:) = pprintIn "*"
 
 instance Show a => MulLit P a where
-  mulLit_ a = pureP $ "mulLit (" ++ show a ++ ")"
+  (>*:) a = pprint2' "mulLit" (show a)
 
 instance Div2 P (Cyc t m (ZqBasic ('PP '(Prime2, k)) i)) where
   type PreDiv2 P (Cyc t m (ZqBasic ('PP '(Prime2, k)) i)) =
     Cyc t m (ZqBasic ('PP '(Prime2, 'S k)) i)
-  div2_ = pureP "div2"
+  div2_ = pprint1 "div2"
 
 instance Div2 P (PNoiseTag h (Cyc t m (ZqBasic ('PP '(Prime2, k)) i))) where
   type PreDiv2 P (PNoiseTag h (Cyc t m (ZqBasic ('PP '(Prime2, k)) i))) =
     PNoiseTag h (Cyc t m (ZqBasic ('PP '(Prime2, 'S k)) i))
-  div2_ = pureP "div2"
+  div2_ = pprint1 "div2"
 
 instance Div2 P (Identity (Cyc t m (ZqBasic ('PP '(Prime2, k)) i))) where
   type PreDiv2 P (Identity (Cyc t m (ZqBasic ('PP '(Prime2, k)) i))) =
     Identity (Cyc t m (ZqBasic ('PP '(Prime2, 'S k)) i))
-  div2_ = pureP "div2"
+  div2_ = pprint1 "div2"
 
 instance Div2 P (CT m (ZqBasic ('PP '(Prime2, k)) i) (Cyc t m' zq)) where
   type PreDiv2 P (CT m (ZqBasic ('PP '(Prime2, k)) i) (Cyc t m' zq)) =
     CT m (ZqBasic ('PP '(Prime2, 'S k)) i) (Cyc t m' zq)
-
-  div2_ = pureP "div2"
+  div2_ = pprint1 "div2"
 
 instance Functor_ P where
-  fmap_ = pureP "fmap"
+  fmap_ = pprint2 "fmap"
 
 instance Applicative_ P where
-  pure_ = pureP "pure"
-  ap_   = pureP "ap"
+  pure_  = pprint1  "pure"
+  (<*:>) = pprintIn "<*>"
 
 instance Monad_ P where
-  bind_ = pureP "bind"
+  (>>=:) = pprintIn ">>="
 
 instance MonadReader_ P where
-  ask_   = pureP "ask"
-  local_ = pureP "local"
+  ask_   = P $ const "ask"
+  local_ = pprint2 "local"
 
 instance MonadWriter_ P where
-  tell_   = pureP "tell"
-  listen_ = pureP "listen"
+  tell_   = pprint1 "tell"
+  listen_ = pprint1 "listen"
+
+instance LinearCyc P rep where
+  type PreLinearCyc P rep = rep
+  type LinearCycCtx P rep t e r s zp = ()
+
+  linearCyc_ _ = pprint1 "linearCyc <FUNC>"
 
 instance SHE P where
 
@@ -110,19 +123,14 @@ instance SHE P where
   type KeySwitchQuadCtx P a gad = ()
   type TunnelCtx        P t e r s e' r' s' zp zq gad = ()
 
-  modSwitchPT_     = pureP   "modSwitchPT"
-  modSwitch_       = pureP   "modSwitch"
-  addPublic_     p = pureP $ "addPublic (" ++ show p ++ ")"
-  mulPublic_     p = pureP $ "mulPublic (" ++ show p ++ ")"
-  keySwitchQuad_ _ = pureP   "keySwitchQuad <HINT>"
-  tunnel_        _ = pureP   "tunnel <HINT>"
-
-instance LinearCyc P rep where
-  type PreLinearCyc P rep = rep
-  type LinearCycCtx P rep t e r s zp = ()
-
-  linearCyc_ _ = pureP "linearCyc <FUNC>"
+  modSwitchPT_     = pprint1  "modSwitchPT"
+  modSwitch_       = pprint1  "modSwitch"
+  addPublic_     p = pprint2' "addPublic" (show p)
+  mulPublic_     p = pprint2' "mulPublic" (show p)
+  keySwitchQuad_ _ = pprint1  "keySwitchQuad <HINT>"
+  tunnel_        _ = pprint1  "tunnel <HINT>"
 
 instance ErrorRate P where
   type ErrorRateCtx P ct z = ()
-  errorRate_ _ = pureP "errorRate"
+  errorRate_ _ = pprint1 "errorRate"
+
