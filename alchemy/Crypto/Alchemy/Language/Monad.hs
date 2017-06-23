2af3140b0225ac95b0a7cc2f8ace9e1964bcb787
Chris Peikert <cpeikert@alum.mit.edu>
2017-06-22 22:25:27 -0400

    object-language style methods

--- alchemy/Crypto/Alchemy/Language/Monad.hs
+++ alchemy/Crypto/Alchemy/Language/Monad.hs
@@ -11,60 +11,54 @@ import Control.Monad.Writer
 
 class Lambda expr => Functor_ expr where
   -- | Object-language analogue of 'fmap'.
-  fmap_ :: (Functor f) => expr e ((a -> b) -> f a -> f b)
+  fmap_ :: (Functor f) => 
+    expr e (Arrow expr a b) -> expr e (f a) -> expr e (f b)
 
--- | Convenient metalanguage version of 'fmap_'.
+-- | Synonym for 'fmap_'.
 infixl 4 <$:>
 (<$:>) :: (Functor_ expr, Functor f) =>
-          expr e (a -> b) -> expr e (f a) -> expr e (f b)
-f <$:> a = fmap_ $: f $: a
+  expr e (Arrow expr a b) -> expr e (f a) -> expr e (f b)
+(<$:>) = fmap_
 
 -- | Symantics for applicative monads: promotes any 'Applicative' to
 -- the object language. (Instances should obey the monad laws.)
 
 class (Functor_ expr) => Applicative_ expr where
   -- | Object-language analogue of 'pure'.
-  pure_ :: (Applicative f) => expr e (a -> f a)
+  pure_ :: (Applicative f) => expr e a -> expr e (f a)
 
   -- | Object-language analogue of '(<*>)'.
-  ap_   :: (Applicative f) => expr e (f (a -> b) -> f a -> f b)
-
--- | Convenient metalanguage version of 'ap_'.
-infixl 4 <*:>
-(<*:>) :: (Applicative_ expr, Applicative f) =>
-  expr e (f (a -> b)) -> expr e (f a) -> expr e (f b)
-f <*:> a = ap_ $: f $: a
+  infixl 4 <*:>
+  (<*:>) :: (Applicative f) => 
+    expr e (f (Arrow expr a b)) -> expr e (f a) -> expr e (f b)
 
 -- | Object-language analogue of 'liftA' (synonym for 'fmap_').
-liftA_ :: (Applicative_ expr, Applicative f) => expr e ((a -> b) -> f a -> f b)
+liftA_ :: (Applicative_ expr, Applicative f) => 
+  expr e (Arrow expr a b) -> expr e (f a) -> expr e (f b)
 liftA_ = fmap_
 
--- | Object-language analogue of 'liftA2' (synonym for 'fmap_').
+-- | Object-language analogue of 'liftA2'.
 liftA2_ :: (Applicative_ expr, Applicative f) =>
-           expr e ((a -> b -> c) -> f a -> f b -> f c)
--- everything must be written in the object language here because
--- that's where liftA2_ lives
-liftA2_ = lam $ lam $ lam $ ap_ $: (fmap_ $: v2 $: v1) $: v0
+  expr e (Arrow expr a (Arrow expr b c)) ->
+  expr e (f a) -> expr e (f b) -> expr e (f c)
+liftA2_ f a b = liftA_ f a <*:> b
 
+-- | Object-language analogue of 'liftA3'.
 liftA3_ :: (Applicative_ expr, Applicative f) =>
-           expr e ((a -> b -> c -> d) -> f a -> f b -> f c -> f d)
-liftA3_ = lam $ lam $ lam $ lam $
-  ap_ $: (ap_ $: (fmap_ $: v3 $: v2) $: v1) $: v0
+  expr e (Arrow expr a (Arrow expr b (Arrow expr c d))) ->
+  expr e (f a) -> expr e (f b) -> expr e (f c) -> expr e (f d)
+liftA3_ f a b c = liftA2_ f a b <*:> c
 
 -- | Symantics for monads: promotes any 'Monad' to the object
 -- language. (Instances should obey the monad laws.)
 
 class (Applicative_ expr) => Monad_ expr where
   -- | Object-language analogue of '(>>=)'.
-  bind_ :: (Monad m) => expr e (m a -> (a -> m b) -> m b)
+  infixl 1 >>=:
+  (>>=:) :: (Monad m) => 
+    expr e (m a) -> expr e (Arrow expr a (m b)) -> expr e (m b)
 
--- | Convenient metalanguage version of 'bind_'.
-infixl 1 >>=:
-(>>=:) :: (Monad_ expr, Monad m) =>
-          expr e (m a) -> expr e (a -> m b) -> expr e (m b)
-a >>=: f = bind_ $: a $: f
-
-return_ :: (Monad_ expr, Monad m) => expr e (a -> m a)
+return_ :: (Monad_ expr, Monad m) => expr e a -> expr e (m a)
 return_ = pure_
 
 -- | Symantics for reader monads: promotes any 'MonadReader' to the
@@ -75,14 +69,15 @@ class (Monad_ expr) => MonadReader_ expr where
   ask_   :: (MonadReader r m) => expr e (m r)
 
   -- | Object-language analogue of 'local'.
-  local_ :: (MonadReader r m) => expr e ((r -> r) -> m a -> m a)
+  local_ :: (MonadReader r m) => 
+    expr e (Arrow expr r r) -> expr e (m a) -> expe e (m a)
 
 -- | Symantics for writer monads: promotes any 'MonadWriter' to the
 -- object language.
 
 class (Monad_ expr) => MonadWriter_ expr where
   -- | Object-language analogue of 'tell'.
-  tell_   :: (MonadWriter w m) => expr e (w -> m ())
+  tell_   :: (MonadWriter w m) => expr e w -> expr e (m ())
 
   -- | Object-language analogue of 'listen_'.
-  listen_ :: (MonadWriter w m) => expr e (m a -> m (a,w))
+  listen_ :: (MonadWriter w m) => expr e (m a) -> expr e (m (a,w))
