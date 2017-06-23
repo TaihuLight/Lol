69ed6922eee99882910dc449c61e8985302f7d6b
Chris Peikert <cpeikert@alum.mit.edu>
2017-06-22 21:51:51 -0400

    progress toward Eval with Arrow style

--- alchemy/Crypto/Alchemy/Interpreter/Eval.hs
+++ alchemy/Crypto/Alchemy/Interpreter/Eval.hs
@@ -18,6 +18,7 @@ import Control.Applicative
 import Control.Monad.Identity
 import Control.Monad.Reader
 import Control.Monad.Writer
+import Data.Coerce
 import Data.Tuple
 
 import Algebra.Additive as Additive
@@ -33,41 +34,51 @@ import Crypto.Alchemy.Language.Pair
 import Crypto.Alchemy.Language.SHE
 import Crypto.Alchemy.Language.String
 
-import Crypto.Lol hiding (String)
+import Crypto.Lol                      hiding (String)
 import Crypto.Lol.Applications.SymmSHE as SHE
 import Crypto.Lol.Types
 
 -- | Metacircular evaluator.
 newtype E e a = E { unE :: e -> a }
-  deriving (Functor)            -- not Applicative; don't want 'pure'!
+  deriving (Functor, Applicative)
 
--- | Evaluate a closed expression (i.e., one not having any unbound
+-- | Evaluate a closed expression (i.e., one having no unbound
 -- variables)
 eval :: E () a -> a
 eval = flip unE ()
 
 instance Lambda E where
-  lam f  = E $ curry $ unE f
-  f $: a = E $ unE f <*> unE a
-  v0     = E snd
+  newtype Arrow E a b = EA { unEA :: (a -> b) } deriving (Functor, Applicative)
+  lam    = coerce . curry . unE
+  f $: a = coerce f <*> a
+  v0     = E   snd
   s a    = E $ unE a . fst
 
-pureE :: a -> E e a
-pureE = E . pure
+arrow1 :: (a -> b) -> Arrow E a b
+arrow1 = coerce
+
+arrow2 :: (a -> b -> c) -> Arrow E a (Arrow E b c)
+arrow2 = coerce
+
+pureE1 :: (a -> b) -> E e (Arrow E a b)
+pureE1 = pure . arrow1
+
+pureE2 :: (a -> b -> c) -> E e (Arrow E a (Arrow E b c))
+pureE2 = pure . arrow2
 
 instance Additive.C a => Add E a where
-  add_ = pureE (+)
-  neg_ = pureE negate
+  add_ = pureE2 (+)
+  neg_ = pureE1 negate
 
 instance Additive.C a => AddLit E a where
-  addLit_ x = pureE (x +)
+  addLit_ x = pureE1 (x +)
 
 instance Ring.C a => Mul E a where
   type PreMul E a = a
-  mul_ = pureE (*)
+  mul_ = pureE2 (*)
 
 instance Ring.C a => MulLit E a where
-  mulLit_ x = pureE (x *)
+  mulLit_ x = pureE1 (x *)
 
 instance (RescaleCyc (Cyc t) (ZqBasic ('PP '(Prime2, 'S k)) i) (ZqBasic ('PP '(Prime2, k)) i),
           Fact m)
@@ -75,7 +86,7 @@ instance (RescaleCyc (Cyc t) (ZqBasic ('PP '(Prime2, 'S k)) i) (ZqBasic ('PP '(P
   type PreDiv2 E (Cyc t m (ZqBasic ('PP '(Prime2, k)) i)) =
     Cyc t m (ZqBasic ('PP '(Prime2, 'S k)) i)
   -- since input is divisible by two, it doesn't matter which basis we use
-  div2_ = pureE rescalePow
+  div2_ = pureE1 rescalePow
 
 instance (RescaleCyc (Cyc t) (ZqBasic ('PP '(Prime2, 'S k)) i) (ZqBasic ('PP '(Prime2, k)) i),
           Fact m)
@@ -83,7 +94,7 @@ instance (RescaleCyc (Cyc t) (ZqBasic ('PP '(Prime2, 'S k)) i) (ZqBasic ('PP '(P
   type PreDiv2 E (PNoiseTag h (Cyc t m (ZqBasic ('PP '(Prime2, k)) i))) =
     PNoiseTag h (Cyc t m (ZqBasic ('PP '(Prime2, 'S k)) i))
   -- since input is divisible by two, it doesn't matter which basis we use
-  div2_ = pureE $ PTag . rescalePow . unPTag
+  div2_ = pureE1 $ PTag . rescalePow . unPTag
 
 instance (RescaleCyc (Cyc t) (ZqBasic ('PP '(Prime2, 'S k)) i) (ZqBasic ('PP '(Prime2, k)) i),
           Fact m)
@@ -91,36 +102,40 @@ instance (RescaleCyc (Cyc t) (ZqBasic ('PP '(Prime2, 'S k)) i) (ZqBasic ('PP '(P
   type PreDiv2 E (Identity (Cyc t m (ZqBasic ('PP '(Prime2, k)) i))) =
     Identity (Cyc t m (ZqBasic ('PP '(Prime2, 'S k)) i))
   -- since input is divisible by two, it doesn't matter which basis we use
-  div2_ = pureE $ Identity . rescalePow . runIdentity
+  div2_ = pureE1 $ Identity . rescalePow . runIdentity
 
 instance (SHE.ModSwitchPTCtx t m' (ZqBasic ('PP '(Prime2, 'S k)) i) (ZqBasic ('PP '(Prime2, k)) i) zq) =>
   Div2 E (CT m (ZqBasic ('PP '(Prime2, k)) i) (Cyc t m' zq)) where
   type PreDiv2 E (CT m (ZqBasic ('PP '(Prime2, k)) i) (Cyc t m' zq)) =
     CT m (ZqBasic ('PP '(Prime2, 'S k)) i) (Cyc t m' zq)
 
-  div2_ = pureE modSwitchPT
+  div2_ = pureE1 modSwitchPT
 
 instance List E where
-  nil_  = pureE []
-  cons_ = pureE (:)
+  nil_  = pure []
+  cons_ = pureE2 (:)
+
+coerceFmap :: forall f a b . (Functor f) => Arrow E (Arrow E a b) (Arrow E (f a) (f b))
+coerceFmap = coerce (fmap :: (a -> b) -> f a -> f b)
 
 instance Functor_ E where
-  fmap_ = pureE fmap
+  -- can't use arrow2 here
+  fmap_ = pure $ EA $ (\f -> EA $ \as -> fmap (unEA f) as)
 
 instance Applicative_ E where
-  pure_ = pureE pure
-  ap_   = pureE (<*>)
+  pure_ = pureE1 pure
+  ap_   = pure $ EA $ (\f -> EA $ \a -> (unEA <$> f) <*> a) --arrow2 (<*>)
 
 instance Monad_ E where
   bind_ = pureE (>>=)
 
 instance MonadReader_ E where
-  ask_   = pureE ask
-  local_ = pureE local
+  ask_   = pure ask
+  local_ = pureE1 local
 
 instance MonadWriter_ E where
-  tell_   = pureE tell
-  listen_ = pureE listen
+  tell_   = pureE1 tell
+  listen_ = pureE1 listen
 
 instance SHE E where
 
@@ -131,18 +146,18 @@ instance SHE E where
   type KeySwitchQuadCtx E (CT m zp (Cyc t m' zq)) gad = (SHE.KeySwitchCtx gad t m' zp zq)
   type TunnelCtx        E t e r s e' r' s' zp zq gad  = (SHE.TunnelCtx t r s e' r' s' zp zq gad)
 
-  modSwitchPT_     = pureE   modSwitchPT
-  modSwitch_       = pureE   modSwitch
-  addPublic_       = pureE . addPublic
-  mulPublic_       = pureE . mulPublic
-  keySwitchQuad_   = pureE . keySwitchQuadCirc
-  tunnel_          = pureE . SHE.tunnel
+  modSwitchPT_     = pureE1 modSwitchPT
+  modSwitch_       = pureE1 modSwitch
+  addPublic_       = pureE1 . addPublic
+  mulPublic_       = pureE1 . mulPublic
+  keySwitchQuad_   = pureE1 . keySwitchQuadCirc
+  tunnel_          = pureE1 . SHE.tunnel
 
 instance (Applicative rep) => LinearCyc E rep where
   type PreLinearCyc E rep = rep
   type LinearCycCtx E rep t e r s zp = (e `Divides` r, e `Divides` s, CElt t zp)
 
-  linearCyc_ f = pureE $ fmap (evalLin f)
+  linearCyc_ f = pureE1 $ fmap (evalLin f)
 
 -- | Uses 'SHE.errorTermUnrestricted' to compute 'errorRate'.
 instance ErrorRate E where
@@ -151,13 +166,13 @@ instance ErrorRate E where
 
   errorRate_ :: forall t m' m z zp zq ct e .
                 (ErrorRateCtx E ct z, ct ~ CT m zp (Cyc t m' zq)) =>
-                SHE.SK (Cyc t m' z) -> E e (ct -> Double)
-  errorRate_ sk = pureE $
+                SHE.SK (Cyc t m' z) -> E e (Arrow E ct Double)
+  errorRate_ sk = pureE1 $
     (/ (fromIntegral $ proxy modulus (Proxy::Proxy zq))) .
     fromIntegral . maximum . fmap abs . SHE.errorTermUnrestricted sk
 
 instance String E where
-  string_ str = E $ const str
+  string_ = pure
 
 instance Pair E where
-  pair_ = E $ const (,)
+  pair_ = pureE2 (,)
